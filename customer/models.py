861038f35bfd8c89a87a7351af66be7a241d0809
rajafarel <114067596+rajafarel@users.noreply.github.com>
2024-01-14 20:41:44 +0700

    Add files via upload

---
+++ customer/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+#Paypal account email and Password below
+#sb-dkbhc27150042@business.example.com
+#0>K[%j?3
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='menu_images/')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    category = models.ManyToManyField('Category', related_name='item')
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    email = models.CharField(max_length=50, blank=True)
+    street = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
+    state = models.CharField(max_length=15, blank=True)
+    zip_code = models.IntegerField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+class OrderModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    items = models.ManyToManyField('MenuItem', related_name='order', blank=True)
+    is_paid = models.BooleanField(default=False)
+    profile = models.ForeignKey('Profile', on_delete=models.SET_NULL, null=True, blank=True, related_name='orders')
+
+    def __str__(self):
+        return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
+    
+
+    
